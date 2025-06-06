e5120ddca5ed4355fbf458a166ef89909ae4c4fc
AgPyae Kyaw <Shwyzee109@gmail.com>
2025-06-06 13:11:35 +0630

    feat: Integrate Google Maps SDK and center map on Yangon with zoom level 10f in MainActivity

---
+++ app/src/main/java/com/example/mapsdktesting/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mapsdktesting
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        val latLng = LatLng(16.8409 , 96.1735)
+        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+    }
+}
